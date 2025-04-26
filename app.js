dd2754781d7a3e8b006b4425c0912ec507b75117
Anis Mhamid <anesmhamed1@gmail.com>
2025-04-27 01:57:42 +0300

    update

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ const receipt = require("./routes/receipt");
 const app = express();
 
 const allowedOrigins = [
-	"http://localhost:5173",
-	"http://localhost:4173",
-	"http://localhost:8209",
-	"https://server-32bo.onrender.com",
-	"https://client-qqq1.vercel.app",
-	"https://client-qqq1-dip2scy3s-anismhamids-projects.vercel.app",
-	process.env.DEV_MODE, // מקור דיפלוי לפיתוח
-	process.env.PROD_MODE, // מקור דיפלוי לייצור
-	process.env.NODE_API, // ה-API המקומי
-	process.env.RENDER_API, // ה-API המפורסם ב-Render
-	process.env.VERCEL_URL, // ה-URL של אפליקציה ב-Vercel
-	process.env.VERCEL_URL_DEVELOPMENT, // ה-URL לפיתוח ב-Vercel
+	// "http://localhost:5173",
+	// "http://localhost:4173",
+	// "http://localhost:8209",
+	// "https://server-32bo.onrender.com",
+	// "https://client-qqq1.vercel.app",
+	// "https://client-qqq1-dip2scy3s-anismhamids-projects.vercel.app",
+	process.env.DEV_MODE,
+	process.env.PROD_MODE,
+	process.env.NODE_API,
+	process.env.RENDER_API,
+	process.env.VERCEL_URL,
+	process.env.VERCEL_URL_DEVELOPMENT,
 ];
 app.use(
 	cors({
