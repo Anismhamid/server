7746707ebe42c0e3509c895f47f18f8e966a1898
Anis Mhamid <anesmhamed1@gmail.com>
2025-04-27 01:42:51 +0300

    ini

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const {limiter} = require("./middlewares/rateLimiter");
 const {logger, logToFile} = require("./utils/logger");
-const {allowedOrigins} = require("./config/allowOrigins");
+// const {allowedOrigins} = require("./config/allowOrigins");
 
 const users = require("./routes/users");
 const carts = require("./routes/carts");
@@ -15,6 +15,20 @@ const receipt = require("./routes/receipt");
 
 const app = express();
 
+const allowedOrigins = [
+	"http://localhost:5173",
+	"http://localhost:4173",
+	"http://localhost:8209",
+	"https://server-32bo.onrender.com",
+	"https://client-qqq1.vercel.app",
+	"https://client-qqq1-dip2scy3s-anismhamids-projects.vercel.app",
+	process.env.DEV_MODE, // מקור דיפלוי לפיתוח
+	process.env.PROD_MODE, // מקור דיפלוי לייצור
+	process.env.NODE_API, // ה-API המקומי
+	process.env.RENDER_API, // ה-API המפורסם ב-Render
+	process.env.VERCEL_URL, // ה-URL של אפליקציה ב-Vercel
+	process.env.VERCEL_URL_DEVELOPMENT, // ה-URL לפיתוח ב-Vercel
+];
 app.use(
 	cors({
 		origin: (origin, callback) => {
@@ -24,8 +38,7 @@ app.use(
 				callback(new Error("Not allowed by CORS"));
 			}
 		},
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		preflightContinue: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 	}),
 );
 
